69ef4d383823ade943c3d5f490be7623623dc043
Carrington-dev <crn96m@gmail.com>
2024-05-14 15:56:29 +0200

    Notes on Dialogue Tutorial

---
+++ app/src/main/java/com/stemgon/diacheckit/MainActivity.java
@@ -0,0 +1,45 @@
+package com.stemgon.diacheckit;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         Button checkDialogue = findViewById(R.id.checkDialogue);
+        checkDialogue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Checking you");
+                builder.setMessage("The set...Button() methods require a title for the button—" +
+                        "supplied by a string resource—and a DialogInterface.OnClickListener");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
